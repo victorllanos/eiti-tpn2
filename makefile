e812660af359a87034bdf9352f84f9c3fc8bfaca
victor llanos <vicllanosho@gmail.com>
2022-09-15 20:08:54 -0300

    modificaciones en el make

--- makefile
+++ makefile
@@ -1,16 +1,23 @@
-SRC_DIR = ./source
-OBJ_DIR = ./build
+SRC_DIR = ./src
+OUT_DIR = ./build
+OBJ_DIR = $(OUT_DIR)/obj
+BIN_DIR = $(OUT_DIR)/bin
 
 SRC_FILES = $(wildcard $(SRC_DIR)/*.c) 
-# SRC_FILES = ./source/main.c ./source/bar.c ./source/bar.c
 OBJ_FILES = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRC_FILES))
-# OBJ_FILES = $(patsubst ./source/%.c, ./build/%.o, $(SRC_FILES))
-# OBJ_FILES = ./build/main.o ./build/bar.o ./build/bar.o
+
+.DEFAULT_GOAL := all
+
+clean: $(OUT_DIR)
+	@echo Borrando binarios
+	@rm -r $(OUT_DIR)
 
 all: $(OBJ_FILES)
-	@gcc $(OBJ_FILES) -o $(OBJ_DIR)/app.out
+	@echo Compilando Enlazando el binario final 
+	@mkdir -p $(BIN_DIR)
+	@gcc $(OBJ_FILES) -o $(BIN_DIR)/app.out
 
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
-	@echo Compilando $< 
+	@echo Compilando $< ...
 	@mkdir -p $(OBJ_DIR)
 	@gcc -o $@ -c $< 
--- src/main.c
+++ src/main.c
@@ -1,11 +1,76 @@
-#include "foo.h"
-#include "bar.h"
+/* Copyright 2022, Laboratorio de Microprocesadores 
+ * Facultad de Ciencias Exactas y Tecnología 
+ * Universidad Nacional de Tucuman
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/** @file main.c
+ **
+ ** @brief Programa principal del tpn2
+ **
+ ** Plantilla para los archivos de codigo fuente de prácticos de las 
+ ** asignaturas Diseño Integrado de Sistemas Emebebidos y Sistemas Embebidos
+ ** de Tiempo Real dictadas en de la Especialización en Integración de
+ ** Sistemas Informaticos de la Univesidad Nacional de Tucumán
+ ** 
+ ** | RV | YYYY.MM.DD | Autor       | Descripción de los cambios              |
+ ** |----|------------|-------------|-----------------------------------------|
+ ** |  1 | 2022.08.27 | evolentini  | Version inicial del archivo             |
+ ** 
+ ** @defgroup plantilla Plantilals de Archivos
+ ** @brief Plantillas de archivos normalizadas
+ ** @{ 
+ */
+
+/* === Inclusiones de cabeceras ============================================ */
+#include "main.h"
 #include <stdio.h>
+
+/* === Definicion y Macros privados ======================================== */
+
+/* === Declaraciones de tipos de datos privados ============================ */
+
+/* === Definiciones de variables privadas ================================== */
+
+/* === Definiciones de variables publicas ================================== */
+
+/* === Declaraciones de funciones privadas ================================= */
+
+/* === Definiciones de funciones privadas ================================== */
+
+/* === Definiciones de funciones publicas ================================== */
 
 int main(void) {
 	printf("Empieza el programa principal\r\n");
-	foo();
-	bar();
 	printf("Termina el programa principal\r\n");
 	return 0;
 }
+/* === Ciere de documentacion ============================================== */
+
+/** @} Final de la definición del modulo para doxygen */
+
